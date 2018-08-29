d7b8b86f93ee4a96b89266bf5a57783254e0bc44
Giancarlo Jung <giancarlo@pipedigital.com>
2018-08-29 14:58:20 -0300

    new quotation mailer created

---
+++ app/Mail/NewQuotation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewQuotation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Quotation $quotation)
+    {
+          $this->quotation = $quotation;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.quotation.new')
+                    ->with([
+                      'user' => $this->quotation->user,
+                      'order' => $this->order,
+                    ]);
+    }
+}
